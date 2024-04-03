c58909dc18cfa8e0fc7dd7b31e760e236725c11e
IvanPanin675 <jovanni.94.van@gmail.com>
2024-04-03 02:58:35 +0200

    backNoFinger

--- src/App.js
+++ src/App.js
@@ -6,31 +6,10 @@ import { Telecommunications } from "./pages/Telecommunications";
 import { SeoContentManager } from "./pages/SeoContentManager";
 import { AboutMe } from "./pages/AboutMe";
 import { StyleLink } from "./components/StyleLink";
-import { useState } from "react";
 
-const App = () => {
-  const [touchCoordinates, setTouchCoordinates] = useState({ x: null, y: null });
-
-  const handleTouchStart = (event) => {
-    const { clientX, clientY } = event.touches[0];
-    setTouchCoordinates({ x: clientX, y: clientY });
-  };
+function App ()  {
   return (
-    <div className="App" onTouchStart={handleTouchStart}>
-      {touchCoordinates.x && touchCoordinates.y && (
-        <img src="./finger.png" alt="finger"
-          className="fingerPrint"
-          style={{
-            display: 'block',
-            position: 'absolute',
-            left: touchCoordinates.x - 25, // ви можете налаштувати положення відбитку пальця
-            top: touchCoordinates.y - 25, // ви можете налаштувати положення відбитку пальця
-            width: '50px',
-            height: '50px',
-            opacity: '0.5',
-          }}
-        />
-      )}
+    <div className="App">
       <header>
         <nav className="Nav-link">
           <StyleLink background='linear-gradient(180deg, #385ced, black)' to="/">AboutMe</StyleLink>
